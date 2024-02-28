b44cea9bdb9471558701f2759691be0015345a11
ziyulin1123 <a0983984816@gmail.com>
2024-02-28 20:39:43 +0800

    大專案 初版 1.已導入 模板CSS JS 2.已製作AllProduct頁面及控制器 3.修改 layout 超連結 導向到 allproduct頁面

---
+++ Project0220/Project0220/Controllers/AllItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project0220.Controllers
+{
+    public class AllItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
