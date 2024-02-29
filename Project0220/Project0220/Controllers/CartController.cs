8262e0b654afcfd07f3d469e37a6769068434ac9
LAPTOP-5D4VOTTK\tiron <tirona520@gmail.com>
2024-02-29 21:27:57 +0800

    建立CartController以及檢視

---
+++ Project0220/Project0220/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project0220.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
