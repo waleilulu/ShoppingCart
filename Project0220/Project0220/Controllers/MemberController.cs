c237fd79e2f95178ef36fb6fb142527af401f258
Chiapei-Chen <claire528816@gmail.com>
2024-03-04 08:57:51 +0800

    會員中心、會員註冊、會員登入畫面

--- Project0220/Project0220/Controllers/MemberController.cs
+++ Project0220/Project0220/Controllers/MemberController.cs
@@ -3,11 +3,17 @@
 namespace Project0220.Controllers
 {
     public class MemberController : Controller
-    {
+    {   //系統預設
         public IActionResult Index()
         {
             return View();
         }
+        //會員中心
+        public IActionResult MemberHome()
+        {
+            return View();
+        }
+        //準備作廢
         public IActionResult Member()
         {
             return View();
@@ -16,6 +22,17 @@ namespace Project0220.Controllers
         public string Member(int id, string Name, string Birthday, string Gender, int Phone, string email, string city, string district, string address, string username, string password)
         {
             return $"OK-Post-{id}-{Name}-{Birthday}-{Gender}-{Phone}-{email}-{city}-{district}-{address}-{username}-{password}";
+        }
+        //會員登入
+        public IActionResult MemberLogin()
+        {
+            return View();
+        }
+
+        //會員註冊
+        public IActionResult MemberRegister()
+        {
+            return View();
         }
     }
 }
