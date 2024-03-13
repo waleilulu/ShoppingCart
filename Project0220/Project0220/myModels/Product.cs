79b3f320ca6d8c99e1b5066b37edc3764437854a
waleilulu <waleilulu@gmail.com>
2024-03-13 09:34:14 +0800

    1. 加入程式碼消除DbContext顯示的警語 2. Products.cs裡的刪除多一條Image 4 3. _Layout: 把首頁的連結加入到Logo上

--- Project0220/Project0220/myModels/Product.cs
+++ Project0220/Project0220/myModels/Product.cs
@@ -22,7 +22,6 @@ public partial class Product
     public string? Image2 { get; set; }
 
     public string? Image3 { get; set; }
-    public string? Image4 { get; set; }
 
     public string? Image4 { get; set; }
 
--- Project0220/Project0220/myModels/ScaffoldEcommerceDbContext.cs
+++ Project0220/Project0220/myModels/ScaffoldEcommerceDbContext.cs
@@ -27,9 +27,12 @@ public partial class ScaffoldEcommerceDbContext : DbContext
 
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
+#pragma warning disable CS1030
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=(local);Database=ECommerceDB;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
+#pragma warning restore CS1030
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
