9f995ccf2d162161fbf4196ddb26a2f627aa1dd8
ziyulin1123 <a0983984816@gmail.com>
2024-03-22 22:36:37 +0800

    目前可以收到驗證碼，但無法比對回存

---
+++ Project0220/Project0220/Migrations/ScaffoldEcommerceDb/20240322034048_AddResetPasswordFieldsToScaffoldEcommerceDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project0220.Migrations.ScaffoldEcommerceDb
+{
+    /// <inheritdoc />
+    public partial class AddResetPasswordFieldsToScaffoldEcommerceDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetPasswordToken",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetPasswordTokenExpiration",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordToken",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordTokenExpiration",
+                table: "Customers");
+        }
+    }
+}
