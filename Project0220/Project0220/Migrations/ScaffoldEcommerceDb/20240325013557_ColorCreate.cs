3c4fd9eb1f7f155f8977dfb4f46d226b73079336
ziyulin1123 <a0983984816@gmail.com>
2024-03-25 22:31:13 +0800

    一鍵式專區功能

---
+++ Project0220/Project0220/Migrations/ScaffoldEcommerceDb/20240325013557_ColorCreate.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project0220.Migrations.ScaffoldEcommerceDb
+{
+    /// <inheritdoc />
+    public partial class ColorCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ__Customer__536C85E423B56005",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "TrackLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SelectedColor",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResetPasswordToken",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Customers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Customer__536C85E423B56005",
+                table: "Customers",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ__Customer__536C85E423B56005",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "TrackLists");
+
+            migrationBuilder.DropColumn(
+                name: "SelectedColor",
+                table: "OrderDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResetPasswordToken",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Customers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Customer__536C85E423B56005",
+                table: "Customers",
+                column: "Username",
+                unique: true,
+                filter: "[Username] IS NOT NULL");
+        }
+    }
+}
