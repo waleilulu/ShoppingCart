ffed0b0c8b95cdbacb9e143dbf97d62e6ce0e7d0
ziyulin1123 <a0983984816@gmail.com>
2024-04-02 21:29:52 +0800

    購物車icon聯動成功

--- Project0220/Project0220/wwwroot/js/Cart_icon_calculate.js
+++ Project0220/Project0220/wwwroot/js/Cart_icon_calculate.js
@@ -1,12 +1,1 @@
-﻿    function calculateTotal() {
-        var totalQuantity = 0;
-        $(".update").each(function () {
-            // 直接獲取並累加數量
-            var quantity = parseInt($(this).val());
-            totalQuantity += quantity;
-        });
-        // 更新總數量顯示到 #totalQuantity 元素
-        $("#totalQuantity").text(totalQuantity);
-        // 同時更新到購物車徽章 .cart-badge 元素
-        $(".cart-badge").text(totalQuantity);
-    }
+﻿
